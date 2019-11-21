da9018350a5cd73bf2be41cc53099be84a4f3b85
Craig Da Silva <craig_344@hotmail.com>
2019-11-21 15:01:01 +0530

    updated UI

---
+++ app/src/main/java/com/example/trial/Canteen1.kt
@@ -0,0 +1,11 @@
+package com.example.trial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Canteen1 : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.canteen_1_items)
+    }
+}
--- app/src/main/java/com/example/trial/MainActivity.kt
+++ app/src/main/java/com/example/trial/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.trial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.canteens)
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.c);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        var canteen1 = findViewById<ImageView>(R.id.imageView)
+        canteen1.setOnClickListener{
+            val addPage = Intent(this, Canteen1::class.java)
+            startActivity(addPage)
+            finish()
+        }
     }
 }
