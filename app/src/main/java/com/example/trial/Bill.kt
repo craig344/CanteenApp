ef050360072ead1a59d8e0cda3fd33d20572ab1e
Craig Da Silva <craig_344@hotmail.com>
2019-11-23 07:48:21 +0530

    Added bill and billItem classes

---
+++ app/src/main/java/com/example/trial/Bill.kt
@@ -0,0 +1,13 @@
+package com.example.trial
+
+class Bill{
+    var id: Int
+    var date : String
+    var status : String
+
+    constructor(id : Int, date : String, status : String) {
+        this.id = id
+        this.date = date
+        this.status = status
+    }
+}
--- app/src/main/java/com/example/trial/BillItem.kt
+++ app/src/main/java/com/example/trial/BillItem.kt
@@ -1,14 +1,16 @@
 package com.example.trial
 
-class Cart {
+class BillItem {
     var id : Int
+    var billId : Int
     var itemName : String
     var itemImage : String
     var itemPrice : Int
     var itemQuantity : Int
 
-    constructor(id : Int, itemName : String, itemImage : String, itemPrice : Int, itemQuantity : Int){
+    constructor(id : Int, billId : Int, itemName : String, itemImage : String, itemPrice : Int, itemQuantity : Int){
         this.id = id
+        this.billId = billId
         this.itemName = itemName
         this.itemImage = itemImage
         this.itemPrice = itemPrice
